350ed3cdfbda8602bb4101cdc726ac3c5c8f4092
dongjae_kim <dongjaekim68@gmail.com>
2021-06-15 19:39:11 +0900

    [06/15] spring boot and jpa usage1 - entity class 2

--- jpashop/jpashop/src/test/java/jpabook/jpashop/MemberRepositoryTest.java
+++ jpashop/jpashop/src/test/java/jpabook/jpashop/MemberRepositoryTest.java
@@ -21,7 +21,7 @@ class MemberRepositoryTest {
     public void testMember() throws Exception {
         //given
         Member member = new Member();
-        member.setUsername("memberA");
+        member.setName("memberA");
 
         //when
         Long savedId = memberRepository.save(member);
@@ -29,7 +29,7 @@ class MemberRepositoryTest {
 
         //then
         assertThat(findMember.getId()).isEqualTo(member.getId());
-        assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
+        assertThat(findMember.getName()).isEqualTo(member.getName());
         assertThat(findMember).isEqualTo(member);
     }
 }
