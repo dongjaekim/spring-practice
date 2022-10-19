4f4d968a06979b93e877234db465b709cbd34218
dongjae_kim <dongjaekim68@gmail.com>
2022-10-19 14:25:40 +0900

    Advisor에 대한 테스트 및 여러 Advisor를 한 프록시에 적용하는 법 테스트

---
+++ proxy/src/test/java/hello/proxy/advisor/MultiAdvisorTest.java
@@ -0,0 +1,76 @@
+package hello.proxy.advisor;
+
+import hello.proxy.common.advice.TimeAdvice;
+import hello.proxy.common.service.ServiceImpl;
+import hello.proxy.common.service.ServiceInterface;
+import lombok.extern.slf4j.Slf4j;
+import org.aopalliance.intercept.MethodInterceptor;
+import org.aopalliance.intercept.MethodInvocation;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.aop.Pointcut;
+import org.springframework.aop.framework.ProxyFactory;
+import org.springframework.aop.support.DefaultPointcutAdvisor;
+import org.springframework.aop.support.NameMatchMethodPointcut;
+
+public class MultiAdvisorTest {
+
+    @Test
+    @DisplayName("multi proxy")
+    void multiAdvisorTest1() {
+        //client -> proxy2 -> advisor2 -> proxy1 -> advisor1 -> target
+
+        // create proxy1
+        ServiceInterface target = new ServiceImpl();
+        ProxyFactory proxyFactory1 = new ProxyFactory(target);
+        DefaultPointcutAdvisor advisor1 = new DefaultPointcutAdvisor(Pointcut.TRUE, new Advice1());
+        proxyFactory1.addAdvisor(advisor1);
+        ServiceInterface proxy1 = (ServiceInterface) proxyFactory1.getProxy();
+
+        // create proxy2, target -> proxy1
+        ProxyFactory proxyFactory2 = new ProxyFactory(proxy1);
+        DefaultPointcutAdvisor advisor2 = new DefaultPointcutAdvisor(Pointcut.TRUE, new Advice2());
+        proxyFactory2.addAdvisor(advisor2);
+        ServiceInterface proxy2 = (ServiceInterface) proxyFactory2.getProxy();
+
+        //execute
+        proxy2.save();
+    }
+
+    @Test
+    @DisplayName("one proxy, multi advisor")
+    void multiAdvisorTest2() {
+        //client -> proxy -> advisor2 -> advisor1 -> target
+
+        DefaultPointcutAdvisor advisor1 = new DefaultPointcutAdvisor(Pointcut.TRUE, new Advice1());
+        DefaultPointcutAdvisor advisor2 = new DefaultPointcutAdvisor(Pointcut.TRUE, new Advice2());
+
+        // create proxy1
+        ServiceInterface target = new ServiceImpl();
+        ProxyFactory proxyFactory1 = new ProxyFactory(target);
+        proxyFactory1.addAdvisor(advisor2);
+        proxyFactory1.addAdvisor(advisor1);
+        ServiceInterface proxy1 = (ServiceInterface) proxyFactory1.getProxy();
+
+        //execute
+        proxy1.save();
+    }
+
+    @Slf4j
+    static class Advice1 implements MethodInterceptor {
+        @Override
+        public Object invoke(MethodInvocation invocation) throws Throwable {
+            log.info("advice1 call");
+            return invocation.proceed();
+        }
+    }
+
+    @Slf4j
+    static class Advice2 implements MethodInterceptor {
+        @Override
+        public Object invoke(MethodInvocation invocation) throws Throwable {
+            log.info("advice2 call");
+            return invocation.proceed();
+        }
+    }
+}
